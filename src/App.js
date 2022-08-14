89d11e2d47606e79f96469678a9d366b9658b44a
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-08-13 20:07:43 -0700

    baby its working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Branch Merge
+          Hey Baby, I think it's working!
         </p>
         <a
           className="App-link"
