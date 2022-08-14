10e82dfbf557c810c0e08422b4c23620a658d45f
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-08-14 02:11:54 -0700

    ill get it

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
           <img src={logo} className="App-logo" alt="logo" />
-        </p>
       </header>
     </div>
   );
