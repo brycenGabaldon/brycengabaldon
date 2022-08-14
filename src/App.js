708013aa3edb99c5f5bfa47fac3ec13c52558b9f
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-08-13 19:38:33 -0700

    Branch Merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Baby, I think it's working!
+          Branch Merge
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          here we fuckin go!
+          Learn React
         </a>
       </header>
     </div>
