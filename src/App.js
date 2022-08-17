cbbb7c711935f7eeb55268a7d3d3f1cee2b80a7f
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-08-16 19:14:12 -0700

    contact form

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ function App() {
         <motion.button
           class="Mandala"
           whileHover={{ rotate: -90 }}
-          whileTap={{ scale: 1.6, rotate: 90 }}
-          
-        >
+          whileTap={{ scale: 1.6, rotate: 90 }}>
           <img src={Mandala} className="App-logo" alt="Mandala" />
         </motion.button>
-        <div></div>
+        
       </header>
+     
     </div>
   );
 }
