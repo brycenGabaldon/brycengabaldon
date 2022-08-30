524e1a58eaa3a1b87ddb73b37dd2defe12fe1e92
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-08-30 03:09:28 -0700

    added cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import Mandala from "./Mandala.png";
-import "./App.css";
+import "./App.scss";
 import { motion } from "framer-motion";
+import LayoutCards from "./layoutCards";
 
 function App() {
   return (
     <div className="App">
+              <LayoutCards />
       <header className="App-header">
         <motion.button
           class="Mandala"
           whileHover={{ rotate: -90 }}
-          whileTap={{ scale: 1.6, rotate: 90 }}>
+          whileTap={{ scale: 1.6, rotate: 90 }}
+        >
           <img src={Mandala} className="App-logo" alt="Mandala" />
         </motion.button>
-        
       </header>
-     
+      <div className="Hello">
+
+      </div>
     </div>
   );
 }
