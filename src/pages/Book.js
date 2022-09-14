6a8a7dd3792e7b3c11f536d12c831499af5fec07
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-09-14 14:20:40 -0700

    rethinking modals

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -3,8 +3,9 @@ import { motion } from 'framer-motion';
 import './pages.scss';
 
 const MODAL_STYLES = {
+
     position: 'fixed',
-    top: '50%',
+    top: '45%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: 'blue',
@@ -28,9 +29,9 @@ export default function Book({ open3, children , onClose3 }) {
 
   return (
     <>
-    <div style={OVERLAY_STYLES}></div>
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} style= {MODAL_STYLES} className="shadow-xl ServiceModal">
-    <button onClick={onClose3} className="shadow-m hover:shadow-xl CloseButton">x</button>
+    <motion.div initial={{ opacity: 0.5 }} animate={{ opacity: 1 }}onClick={onClose3} style={OVERLAY_STYLES}></motion.div>
+    <motion.div initial={{ y: '-150%', x: '-50%' }} animate={{ y: '-58%', x: '-50%'}}  style= {MODAL_STYLES} className="shadow-xl ServiceModal">
+
     {children}</motion.div>
     </>
   )
