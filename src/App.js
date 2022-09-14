c974746f95f02c4a5cb3f0da5bd4dc15562a8437
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-09-13 23:38:02 -0700

    sizing for smaller screens and shadows

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
     <div className="App">
       <div className="ButtonContainer">
 
-          <button class="Button Big"> Services</button>
-          <button class="Button Small"> Contact</button>
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Big"> Services</button>
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small"> Contact</button>
 
-          <button class="Button Small"> Book Appointment</button>
-          <button class="Button Big"> About us</button>
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small"> Book Appointment</button>
+          <button class="shadow-lg hover:shadow-gray-500/50 Button Big"> About us</button>
 
       </div>
     </div>
