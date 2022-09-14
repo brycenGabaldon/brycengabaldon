13fed115f96c559694c815e191b8baea73af048e
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-09-13 22:50:58 -0700

    Reset HTML JS only

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import Mandala from "./Mandala.png";
 import "./App.scss";
-import { motion } from "framer-motion";
-import LayoutCards from "./layoutCards";
 
 function App() {
   return (
     <div className="App">
-              <LayoutCards />
-      <header className="App-header">
-        <motion.button
-          class="Mandala"
-          whileHover={{ rotate: -90 }}
-          whileTap={{ scale: 1.6, rotate: 90 }}
-        >
-          <img src={Mandala} className="App-logo" alt="Mandala" />
-        </motion.button>
-      </header>
-      <div className="Hello">
+      <div className="ButtonContainer">
+
+          <button class="Button Big"> Services</button>
+          <button class="Button Small"> Contact</button>
+
+          <button class="Button Small"> Book Appointment</button>
+          <button class="Button Big"> About us</button>
 
       </div>
     </div>
