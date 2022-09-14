af743ac2d73bbdbd2da2d5f7d23741352c4e9f3c
brycenGabaldon <47164000+brycenGabaldon@users.noreply.github.com>
2022-09-14 00:39:27 -0700

    added modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import "./App.scss";
+import Modal from "./Modal";
+import React, { useState } from "react";
 
-function App() {
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="App">
-      <div className="ButtonContainer">
+    <>
+      <div className="App">
+        <div className="ButtonContainer">
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Big" onClick={() => setIsOpen(true)}>
+            {" "}
+            Services
+          </button>
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small">
+            {" "}
+            Contact
+          </button>
+          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small">
+            {" "}
+            Book Appointment
+          </button>
+          <button class="shadow-lg hover:shadow-gray-500/50 Button Big">
+            {" "}
+            About us
+          </button>
 
-          <button class="shadow-lg  hover:shadow-gray-500/50 Button Big"> Services</button>
-          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small"> Contact</button>
-
-          <button class="shadow-lg  hover:shadow-gray-500/50 Button Small"> Book Appointment</button>
-          <button class="shadow-lg hover:shadow-gray-500/50 Button Big"> About us</button>
-
+          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            Modal
+            </Modal>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default App;
