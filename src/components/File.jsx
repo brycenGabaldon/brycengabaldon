b56c0b439fbcc161df374ab30df56cbfbfd718b3
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-09 08:34:40 -0700

    commit

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -8,15 +8,15 @@ export default function Buttons() {
   return (
     <div className='Roww'>
       {data.icons.map((icon, key) => {
-        return <div className="Spacing" href={icon.ref}>
+        return <div className="Spacing">
             <button className="buttons1 aspect-w-1 aspect-h-1" >
-            <motion.div
-        className={icon.iconClass} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
+            <motion.button
+        className={icon.iconClass} href={icon.ref} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
         style={icon.style}>
         <div className={icon.labelClass} >
       {icon.name}</div>
 
-      </motion.div>
+      </motion.button>
       </button>
     </div>
       })}
