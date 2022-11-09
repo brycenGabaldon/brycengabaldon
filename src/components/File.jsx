0af5863b3180addd992a39ebffedcec1646f01e6
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-09 11:02:32 -0700

    fontaawesome

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { data } from '../iconData'
 import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Buttons() {
 
@@ -11,11 +13,13 @@ export default function Buttons() {
         return <div className="Spacing">
             <button className="buttons1 aspect-w-1 aspect-h-1" >
             <motion.button
-        className={icon.iconClass} href={icon.ref} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
+        className={icon.iconClass} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
         style={icon.style}>
+            <a href={icon.ref}>
+            <FontAwesomeIcon icon={icon.iconImg} style={icon.iconStyles}/>
+          </a>
         <div className={icon.labelClass} >
       {icon.name}</div>
-
       </motion.button>
       </button>
     </div>
--- src/iconData.js
+++ src/iconData.js
@@ -4,19 +4,21 @@ export const data = {
       
     key : 1,
   name : "Phone",
-  iconImg : "iconName",
+  iconImg : "faSolid",
   label : "Phone",
   ref : "tel:602-816-1523",
   onClick : "Function",
   labelClass : "buttonLabel",
   active : "active",
   visible : "visible",
-  iconClass : "Icons1 aspect-w-1 aspect-h-1 shadow-lg  hover:shadow-gray-500/50 Button"
+  iconClass : "Icons1 aspect-w-1 aspect-h-1 shadow-lg  hover:shadow-gray-500/50 Button",
+  iconStyles : "Color: white"
+
   },
   {
     key : 2,
   name : "other",
-  iconImg : "iconName",
+  iconImg : "coffee",
   label : "no?!",
   ref : "refLink",
   onClick : "Function",
@@ -29,7 +31,7 @@ export const data = {
 {
   "key" : 3,
   "name" : "one",
-  "iconImg" : "iconName",
+  "iconImg" : "twitter",
   "label" : "yes?!",
   "ref" : "refLink",
   "onClick" : "Function",
