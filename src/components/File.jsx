28bc10a6086b3f4b6032c486743887708c3bfe3d
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-10 14:56:06 -0700

    still testing

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { data } from '../iconData'
 import { motion } from 'framer-motion';
 
+
 export default function Buttons() {
+
     
   return (
     <div className='Roww'>
-      {data.icons.map((iconn, key) => {
-        return <div className="Spacing" key={iconn.key}>
+      {data.icons.map((icon, key) => {
+        return <div className="Spacing">
             <button className="buttons1 aspect-w-1 aspect-h-1" >
             <motion.button
-        className={iconn.iconClass} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
-        style={iconn.style}>
-            <a href={iconn.ref}>
-          </a>
-        <div className={iconn.labelClass} >
-      {iconn.name}</div>
+        className={icon.iconClass} whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.95 }}
+        style={icon.style}>
+        <div className={icon.labelClass} >
+      {icon.name}</div>
       </motion.button>
       </button>
     </div>
