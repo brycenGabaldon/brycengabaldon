c7a2cf97e553834fead21e1253bcb6dd3536dacb
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-16 20:39:46 -0700

    commit

--- src/components/FolderIcons.jsx
+++ src/components/FolderIcons.jsx
@@ -14,11 +14,11 @@ export default function FolderStatus({isActive, handleClick, folders}) {
 console.log("before function")
 
   return true
-    ? [<motion.div onClick={()=>handleClick()} className="folders" initial={{scale:3}} animate={{ scale: 1}} ><OpenFolder id="closed" key={Math.floor(1 + Math.random() * 10000)} isActive={isActive} handleClick={()=>handleClick(folders.id)}/></motion.div>]
+    ? [<motion.div onClick={()=>handleClick()} className="folders" initial={{scale:3}} animate={{ scale: 1}} ><OpenFolder id="closed" key={Math.floor(1 + Math.random() * 10000)} isActive={isActive} handleClick={()=>handleClick()}/></motion.div>]
 
     : 
     
-    [<motion.div  className="folders" initial={{ position: "fixed", top: "20%", left: "50%" }} animate={{ scale: 3, position: "fixed", top: "40%", left: "40%", zIndex: "1000"}} ><OpenFolder   id="open" key={Math.floor(1 + Math.random() * 10000)} isActive={isActive} /><ModalPortal onClick={()=>handleClick(folders.id)} isActive={isActive} /></motion.div>];
+    [<motion.div  className="folders" initial={{ position: "fixed", top: "20%", left: "50%" }} animate={{ scale: 3, position: "fixed", top: "40%", left: "40%", zIndex: "1000"}} ><OpenFolder   id="open" key={Math.floor(1 + Math.random() * 10000)} isActive={isActive} /><ModalPortal onClick={()=>handleClick()} isActive={isActive} /></motion.div>];
 
     
 }
