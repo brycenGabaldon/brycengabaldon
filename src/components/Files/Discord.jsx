9250eeb7dbaae47fc73a9e20648315e91ca3efb7
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-19 16:25:45 -0700

    color and sizing discord

--- src/components/Files/Discord.jsx
+++ src/components/Files/Discord.jsx
@@ -12,7 +12,7 @@ const Discord = () => {
     return (
       <div className="DiscordView">
 
-<iframe title="discord" src="https://e.widgetbot.io/channels/1043659503259877437/1043659504157470772" height="92%" width="100%"></iframe>
+<iframe title="discord" src="https://e.widgetbot.io/channels/1043659503259877437/1043659504157470772" height="100%" width="100%"></iframe>
 
         </div>
  
