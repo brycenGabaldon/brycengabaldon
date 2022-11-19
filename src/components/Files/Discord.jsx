721beba3bdef2f219a697df441802c31806a2e91
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-19 16:55:17 -0700

    discord

--- src/components/Files/Discord.jsx
+++ src/components/Files/Discord.jsx
@@ -1,22 +1,34 @@
+import * as React from 'react'
+import WidgetBot, { API } from '@widgetbot/react-embed'
 
-import React from "react";
+class Discord extends React.Component {
+  api: API
 
-import "./Files.scss"
+  onAPI(api: API) {
+    this.api = api
+    api.on('signIn', user => {
+      console.log(`Signed in as ${user.name}`, user)
+    })
+  }
 
+  handleClick() {
+    this.api.emit('sendMessage', `Hello world! from \`@widgetbot/react-embed\``)
+  }
 
-
-const Discord = () => {
-
-
-    
+  render() {
     return (
-      <div className="DiscordView">
+      <div className='DiscordView'>
 
-<iframe title="discord" src="https://e.widgetbot.io/channels/1043659503259877437/1043659504157470772" height="100%" width="100%"></iframe>
-
-        </div>
- 
-    );
-  };
+        <WidgetBot
+          server="1043659503259877437"
+          channel="1043659504157470772"
+          onAPI={this.onAPI.bind(this)}
+          height="100%"
+          width="100%"
+        />
+      </div>
+    )
+  }
+}
 
   export default Discord;
